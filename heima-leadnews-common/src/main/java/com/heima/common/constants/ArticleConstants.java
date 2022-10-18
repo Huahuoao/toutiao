daa700c64530d7f9ea5418cc5f11408e80318acd
Huahuoao <2294198058@qq.com>
2022-10-18 23:52:31 +0800

    微服务let us go

---
+++ heima-leadnews-common/src/main/java/com/heima/common/constants/ArticleConstants.java
@@ -0,0 +1,13 @@
+package com.heima.common.constants;
+
+/**
+ * @作者 花火
+ * @创建日期 2022/10/18 16:49
+ */
+public class ArticleConstants {
+
+        public static final Short LOADTYPE_LOAD_MORE = 1;
+        public static final Short LOADTYPE_LOAD_NEW = 2;
+        public static final String DEFAULT_TAG = "__all__";
+
+}
---
+++ heima-leadnews-test/freemarker-demo/src/main/java/com/heima/freemarker/FreemakerApplication.java
@@ -0,0 +1,15 @@
+package com.heima.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @作者 花火
+ * @创建日期 2022/10/18 23:13
+ */
+@SpringBootApplication
+public class FreemakerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreemakerApplication.class,args);
+    }
+}
